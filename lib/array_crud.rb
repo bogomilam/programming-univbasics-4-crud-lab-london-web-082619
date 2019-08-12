71ee6ea400e33677da3d1602091d36302d69ae74
bogomilmihaylov <bogomilmihaylov@live.com>
2019-08-12 18:26:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Bogo","Diana","Rada","Alex"]
+ family = ["Bogo","Diana","Rada","Alex"]
 end
 
 def add_element_to_end_of_array(array, element)
